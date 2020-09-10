259b4099299b6afc74eb87b039fd9c1d9bc8c059
Andre-Weber <andreew@al.insper.edu.br>
2020-09-10 10:50:31 -0300

    fixed doc: descriptions, titles, tags and http responses

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
 # mock
 import json
 from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
 
 with open('mock.json', 'r') as f: db = json.load(f)
 #
 
 from typing import Optional
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel
 
 tags_metadata = [
     {
-        "name": "list tasks",
-        "description": "List tasks and filter by done parameter.",
-    },
-    {
-        "name": "add task",
-        "description": "Add task to the db",
-    },
-    {
-        "name": "remove task",
-        "description": "Remove task from db",
-    },
-    {
-        "name": "update task",
-        "description": "Update task from db",
-    },
+        "name": "Tasks",
+        "description": "Tasks related features.",
+    }
 ]
 
-app = FastAPI(title = "Megadados-api", openapi_tags = tags_metadata)
+app = FastAPI(title = "Megadados-api", openapi_tags = tags_metadata, description="FIrst touch with FastApi")
 
 def _list_tasks_logic(done: int):
     tasks = {}
@@ -58,20 +45,24 @@ class Task(BaseModel):
 def read_root():
     return {"Hello" : "World"}
 
-@app.get("/list/", tags = ["list tasks"], response_model = dict)
+@app.get("/list/", tags = ["Tasks"], response_model = dict, summary = 'List all tasks.')
 def list_tasks():
+    """
+    List all existent tasks and their information.
+    """
     return db
 
-@app.get("/list/{done}", tags = ["list tasks"], response_model = dict)
-def list_tasks_filter(done: int):
+@app.get("/list/{done}", tags = ["Tasks"], response_model = dict, summary = 'List tasks based on status.')
+def list_tasks_filter(done: bool):
     """
-    Create an item with all the information:
-
+    List tasks based on information "done":
     - **done**: if the task is done
+     - true: done tasks.
+     - false: not done tasks.
     """
     return _list_tasks_logic(done)
 
-@app.post("/add-task/", response_model = Task, tags = ["add task"])
+@app.post("/add-task/", response_model = Task, tags = ["Tasks"], summary = 'Add a new task.')
 def add_task(task: Task):
     """
     Create an item with all the information:
@@ -80,41 +71,45 @@ def add_task(task: Task):
     - **description**: a long description about the task
     - **done**: if the task is done
     """
-
     name_idx = _db_has_name(task.name)
-    if name_idx != None: return JSONResponse(status_code = 400, content = {"message" : "name already exists"})
+    if name_idx != None: 
+        raise HTTPException(status_code = 400, detail = {"message" : "name already exists"})
 
     db["tasks"].append(jsonable_encoder(task))
     _write_json()
     return task
 
-@app.delete("/remove-task/{name}", response_model = str, tags = ["remove task"])
+@app.delete("/remove-task/{name}", response_model = str, tags = ["Tasks"], summary = 'Delete a task.')
 def remove_task(name: str):
     """
-    Create an item with all the information:
+    Delete a task based on information "name":
 
     - **name**: each tasks must have a name
     """
     name_idx = _db_has_name(name)
-    if name_idx == None: return JSONResponse(status_code = 400, content = {"message" : "name doesn't exists"})
-
-    del db["tasks"][name_idx]
-
+    if name_idx == None: 
+        raise HTTPException(status_code = 400, detail = {"message" : "name doesn't exists"})
+    else:
+        del db["tasks"][name_idx]
     _write_json()
     return name
 
-@app.put("/update-task/", response_model = Task, tags = ["update task"])
+@app.put("/update-task/", response_model = Task, tags = ["Tasks"], summary = 'Update an information from task.')
 def update_task(task: Task):
     """
-    Create an item with all the information:
+    Update an information ("description" or "done") from a task given its name:
 
-    - **name**: each tasks must have a name
+    - **name**: each tasks must have a name (specify the name of the task you want to update)
+
+    You can choose which informations you want to update, exclude the ones you don't:
+
     - **description**: a long description about the task
     - **done**: if the task is done
     """
 
     name_idx = _db_has_name(task.name)
-    if name_idx == None: return JSONResponse(status_code = 400, content = {"message" : "name doesn't exists"})
+    if name_idx == None: 
+        raise HTTPException(status_code = 400, detail = {"message" : "name doesn't exists"})
     
     stored_item_data = db["tasks"][name_idx]
     stored_item_model = Task(**stored_item_data)
